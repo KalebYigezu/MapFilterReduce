655b242e77bb1dbfff6e39de47e3343527f54308
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-11 12:09:55 -0700

    Update MapFilterReduce.py

--- MapFilterReduce.py
+++ MapFilterReduce.py
@@ -1,1 +1,16 @@
+from functools import reduce
 
+
+
+nums = [0,1,2,3,4,5,6,7,8,9,10]
+
+evens = list(filter(lambda x: x % 2 == 0, nums))
+
+divide = list(map(lambda x: int(x / 2), evens))
+
+sum = reduce(lambda x, y: x + y, divide)
+
+print("Nums :  ", nums)
+print("Evens :  ",evens)
+print("Divide :  ",divide)
+print("Sum :  ",sum)
