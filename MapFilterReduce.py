47a773bef4cdfc2e4767aa549ce7211e04de0717
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-28 00:25:15 -0700

    Update MapFilterReduce.py

--- MapFilterReduce.py
+++ MapFilterReduce.py
@@ -14,3 +14,33 @@ print("Nums :  ", nums)
 print("Evens :  ",evens)
 print("Divide :  ",divide)
 print("Sum :  ",sum)
+
+------------------------------------------------------
+
+from functools import reduce
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def even(n):
+    if n % 2 == 0:
+        return n
+    else:
+        return n + 1
+
+def gfive(n):
+    if n > 5:
+        return 1
+
+def add(x, y):
+    return x + y
+
+
+mo = list(map(even, l))
+print("Even = ", mo)
+
+fo = list(filter(gfive, mo))
+print("Greater Than = ", fo)
+
+ro = reduce(add, fo)
+print(ro)
